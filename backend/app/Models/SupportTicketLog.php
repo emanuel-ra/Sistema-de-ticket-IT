cbe017e3048a1b383c978a7a8d2cb34bb374f774
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-08-11 14:47:04 -0600

    Created: Model, Migration and Seeder to Support Tickets Logs

---
+++ backend/app/Models/SupportTicketLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SupportTicketLog extends Model
+{
+    use HasFactory;
+}
