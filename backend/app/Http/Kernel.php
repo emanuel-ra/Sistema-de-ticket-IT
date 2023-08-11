e5314c14812feae1d8024e05e56a5123f70d89f3
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-08-11 14:01:52 -0600

    Routes Created

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -42,6 +42,11 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            // AUTH WITH SANCTUM
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\AuthenticationController as v1Auth;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,45 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * *  V1 ROUTES 
+*/
+Route::prefix('vi')->group(function(){
+
+    /**
+     * * FREE ROUTES
+     */
+     // * AUTHENTICATION
+    Route::prefix('auth')->group(function(){
+        Route::post('login',[v1Auth::class,'login']);
+    });
+
+    // * TICKETS FROM CLIENT SIDE
+    Route::prefix('tickets')->group(function(){
+        //Route::post('request',[v1Auth::class,'store']);
+    });
+
+
+     /**
+      * * PROTECTED ROUTES
+      */    
+    Route::middleware('auth:sanctum')->group(function(){
+
+        // AUTH
+        Route::prefix('auth')->group(function(){
+            Route::post('logout',[App\Http\Controllers\Api\AuthController::class,'logout']);
+        });
+
+        // TICKETS
+        Route::prefix('admin')->group(function(){
+            
+            Route::prefix('tickets')->group(function(){
+
+            });
+            
+        });
+    });
+    
+});
+
