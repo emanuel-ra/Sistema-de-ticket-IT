50cbd11dbbb2e51af780e51cc26a5105b9d23948
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-08-12 11:57:04 -0600

    Edited: Navbar is fixed now

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -5,16 +5,18 @@ function Navbar() {
     
   return (
     <>
-    <nav>
-        <div className="nav-wrapper blue darken-4" style={styles}>
-            <a href="#" className="brand-logo">Sistema de Tickets</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Option 1</a></li>
-                <li><a href="#">Option 2</a></li>
-                <li><a href="#">Option 3</a></li>
-            </ul>
-        </div>
-    </nav>
+    <div className="navbar-fixed">
+        <nav>
+            <div className="nav-wrapper blue darken-4" style={styles}>
+                <a href="#" className="brand-logo">Sistema de Tickets</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    {/* <li><a href="#">Option 1</a></li>
+                    <li><a href="#">Option 2</a></li>
+                    <li><a href="#">Option 3</a></li> */}
+                </ul>
+            </div>
+        </nav>
+    </div>
     </>
   )
 }
